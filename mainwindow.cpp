0a7651cc1ca5fcddd7041997bf8356ab19197a57
Mathew MacDougall <mmacd97@gmail.com>
2017-04-07 18:45:23 -0700

    added control buttons and made splitter handle visible

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,9 +8,25 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 
     setWindowTitle(tr("Chaos Pendulum"));
+
+    // sets so that the PendulumWidget takes up as much
+    // space as possible initially in the splitter
+    ui->splitter->setStretchFactor(0, 1);
+    ui->splitter->setStretchFactor(1, 0);
+
+    // set stylesheet for the splitter handle
+    // basically makes the splitter visible
+    this->setStyleSheet("QSplitter::handle{"
+                        "background: qlineargradient(x1:0, y1:0, x2:1, y2:1,"
+                        "stop:0 #eee, stop:1 #ccc);"
+                        "border: 1px solid #777;"
+                        "width: 5px;"
+                        "margin-top: 2px;"
+                        "margin-bottom: 2px;"
+                        "border-radius: 4px;"
+                        "}");
 }
 
 MainWindow::~MainWindow() {
     delete ui;
 }
-
